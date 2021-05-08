de128cd7eed1ee3f9436d8e079f3b8b9365889ed
Azat Vekilov <wekilow87@gmail.com>
2021-05-08 12:13:41 -0500

    Update ViewDeck.js

--- Project_Flashcards_Qualified_1/src/viewDeck/ViewDeck.js
+++ Project_Flashcards_Qualified_1/src/viewDeck/ViewDeck.js
@@ -3,24 +3,6 @@ import { useHistory, useParams, Link } from "react-router-dom";
 import { deleteDeck, readDeck } from "../utils/api/index";
 import CardList from "./CardList"
 
-/*
- The Deck screen has the following features:
-    x The path to this screen should include the deckId (i.e., /decks/:deckId).
-    x There is a breadcrumb navigation bar with a link to home / followed by the name of the deck (e.g., Home/React Router).
-    x The screen includes the deck name (e.g., "React Router") and deck description (e.g., "React Router is a collection of navigational components that compose declaratively in your application").
-    x The screen includes "Edit", "Study", "Add Cards", and "Delete" buttons. Each button takes the user to a different destination, as follows:
-        | "Edit" | Edit Deck Screen |
-        | "Study" | Study screen |
-        | "Add Cards" | Add Card screen |
-        | "Delete" | Shows a warning message before deleting the deck]
-            When the user clicks the "Delete" button associated with a card, a warning message is shown and the user can click "OK" or "Cancel". If the user clicks "OK", the card is deleted.
-    - Each card in the deck:
-        - is listed on the page under the "Cards" heading.
-        - shows a question and the answer to the question.
-        - has an “Edit” button that takes the user to the Edit - Card screen when clicked.
-        - has a “Delete” button that allows that card to be deleted.
-*/
-
 function ViewDeck() {
     const { deckId } = useParams();
     const history = useHistory();
