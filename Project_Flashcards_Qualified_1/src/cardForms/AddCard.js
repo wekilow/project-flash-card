08daefaf65a030f4a1e64359cdedec65cb70eeda
Azat Vekilov <wekilow87@gmail.com>
2021-05-08 12:10:14 -0500

    Update AddCard.js

--- Project_Flashcards_Qualified_1/src/cardForms/AddCard.js
+++ Project_Flashcards_Qualified_1/src/cardForms/AddCard.js
@@ -3,16 +3,6 @@ import { Link, useParams } from "react-router-dom";
 import { createCard, readDeck } from "../utils/api/index";
 import CardForm from "./CardForm";
 
-/*
- The Add Card screen has the following features:
-    - The path to this screen should include the deckId (i.e., /decks/:deckId/cards/new).
-    - There is a breadcrumb navigation bar with a link to home /, followed by the name of the deck to which the cards are being added, and finally the text Add Card (e.g., Home/React Router/Add Card).
-    - The screen displays the "React Router: Add Card" deck title.
-    - A form is shown with the "front" and "back" fields for a new card. Both fields use a <textarea> tag that can accommodate multiple lines of text.
-    - If the user clicks "Save", a new card is created and associated with the relevant deck. Then the form is cleared and the process for adding a card is restarted.
-    - If the user clicks "Done", the user is taken to the Deck screen.
-*/
-
 function AddCard() {
     const { deckId } = useParams();
 
@@ -36,7 +26,6 @@ function AddCard() {
         event.preventDefault();
         await createCard(deckId, formData);
         setFormData({...initialForm})
-        //history.push(`/decks/${deckId}`);
     }
 
     const done =
