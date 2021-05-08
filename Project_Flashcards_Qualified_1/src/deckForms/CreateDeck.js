1c5cf9b7f49f797d16da18675870d7918d1a623c
Azat Vekilov <wekilow87@gmail.com>
2021-05-08 12:11:14 -0500

    Update CreateDeck.js

--- Project_Flashcards_Qualified_1/src/deckForms/CreateDeck.js
+++ Project_Flashcards_Qualified_1/src/deckForms/CreateDeck.js
@@ -3,17 +3,6 @@ import { Link, useHistory } from "react-router-dom";
 import { createDeck } from "../utils/api/index";
 import DeckForm from "./DeckForm"
 
-/*
- The Create Deck screen has the following features:
-    The path to this screen should be /decks/new.
-        x There is a breadcrumb navigation bar with a link to home / followed by the text Create Deck (i.e., Home/Create Deck).
-            - A form is shown with the appropriate fields for creating a new deck.
-            - The name field is an <input> field of type text.
-            - The description field is a <textarea> field that can be multiple lines of text.
-            -  If the user clicks "submit", the user is taken to the Deck screen.
-            - If the user clicks "cancel", the user is taken to the Home screen.
-*/
-
 function CreateDeck() {
     const history = useHistory();
     const initialForm = {
